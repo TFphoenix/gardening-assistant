ba711a5402c245f9cf237425bed1938bae362b5a
Teodor <teodor@mihaescu.info>
2020-11-08 23:32:13 +0200

    Added HealthCameraViewModel and complemented color palette.

---
+++ ga-forms/ga-forms/ga-forms/ViewModels/HealthCameraViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ga_forms.ViewModels
+{
+    class HealthCameraViewModel : ViewModel
+    {
+        public HealthCameraViewModel()
+        {
+            Title = "Health Camera Page";
+        }
+    }
+}
