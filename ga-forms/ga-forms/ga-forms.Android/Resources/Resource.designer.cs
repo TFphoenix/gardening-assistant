c2586e31918fd4a9ca56b93586633b526ca53de0
Gabriela Burtan <burtangabriela@gmail.com>
2020-11-09 16:27:40 +0200

    Designed HealthResultsPage

--- ga-forms/ga-forms/ga-forms.Android/Resources/Resource.designer.cs
+++ ga-forms/ga-forms/ga-forms.Android/Resources/Resource.designer.cs
@@ -11009,31 +11009,37 @@ namespace ga_forms.Droid
 			public const int notify_panel_notification_icon_bg = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int tab_back = 2131165306;
+			public const int save = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int tab_decorate = 2131165307;
+			public const int save64 = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int tab_health = 2131165308;
+			public const int tab_back = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int tab_home = 2131165309;
+			public const int tab_decorate = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int tab_plants = 2131165310;
+			public const int tab_health = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int tab_upload = 2131165311;
+			public const int tab_home = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int tooltip_frame_dark = 2131165312;
+			public const int tab_plants = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int tooltip_frame_light = 2131165313;
+			public const int tab_upload = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int user64 = 2131165314;
+			public const int tooltip_frame_dark = 2131165314;
+			
+			// aapt resource value: 0x7F070083
+			public const int tooltip_frame_light = 2131165315;
+			
+			// aapt resource value: 0x7F070084
+			public const int user64 = 2131165316;
 			
 			static Drawable()
 			{
