1166abeffa10c2a98c2a222bcb674b5add7f5424
Gabriela Burtan <burtangabriela@gmail.com>
2021-01-04 22:05:07 +0200

    Added view components to DecoratePage

--- ga-forms/ga-forms/ga-forms/Views/DecoratePage.xaml.cs
+++ ga-forms/ga-forms/ga-forms/Views/DecoratePage.xaml.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ga_forms.Common;
+using ga_forms.ViewModels;
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ga_forms.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class DecoratePage : ContentPage
-{
-    public DecoratePage()
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DecoratePage : ContentPage
     {
-        InitializeComponent();
+        public DecoratePage()
+        {
+            InitializeComponent(); 
+            CameraButton.Margin = new Thickness(0, 0, 0, 30);
+        }
+        private void CameraView_OnAvailable(object sender, bool e)
+        {
+            CameraButton.IsEnabled = e;
+        }
+
+        private void CameraView_MediaCaptured(object sender, MediaCapturedEventArgs args)
+        {
+            (BindingContext as HealthCameraViewModel)?.OnSnapshot(args.Path);
+        }
+
+        private void CameraView_MediaCaptureFailed(object sender, string e)
+        {
+            //TODO: Handle error
+        }
     }
-}
 }
