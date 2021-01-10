2743d7023b0e06863c6bf9ef605fefa219be3201
Gabriela Burtan <burtangabriela@gmail.com>
2021-01-10 17:38:39 +0200

    Added basic view for DecorateResultsPage

---
+++ ga-forms/ga-forms/ga-forms/Views/DecorateResultsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ga_forms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DecorateResultsPage : ContentPage
+    {
+        public DecorateResultsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
