b9217020adddda32d5bff272b964fdd708e8f2ad
Teodor <teodor@mihaescu.info>
2021-01-15 21:40:58 +0200

    Implemented Color Segmentation algorithm

---
+++ ga-forms/ga-forms/ga-forms/Models/ImageProcessing/Algorithms/ColorSegmentation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+
+namespace ga_forms.Models.ImageProcessing.Algorithms
+{
+    class ColorSegmentation : IAlgorithm
+    {
+        public SKBitmap ProcessingImage { get; set; }
+        public SKBitmap ProcessedImage { get; set; }
+        public SKColor Color { get; set; }
+        public float EstimatedDistance { get; set; }
+
+        public ColorSegmentation()
+        {
+            Color = new SKColor(127, 127, 127);
+            EstimatedDistance = 10f;
+        }
+
+        public void Execute()
+        {
+            for (int x = 0; x < ProcessingImage.Width; x++)
+            {
+                for (int y = 0; y < ProcessingImage.Height; y++)
+                {
+                    SKColor pixel = ProcessingImage.GetPixel(x, y);
+                    float distance = (float)Math.Sqrt(
+                        ((int)pixel.Red - (int)Color.Red) * ((int)pixel.Red - (int)Color.Red) +
+                        ((int)pixel.Green - (int)Color.Green) * ((int)pixel.Green - (int)Color.Green) +
+                        ((int)pixel.Blue - (int)Color.Blue) * ((int)pixel.Blue - (int)Color.Blue)
+                    );
+
+                    ProcessedImage.SetPixel(x, y,
+                        distance <= EstimatedDistance
+                            ? new SKColor(255, 255, 255, pixel.Alpha)
+                            : new SKColor(0, 0, 0, pixel.Alpha));
+                }
+            }
+        }
+    }
+}
